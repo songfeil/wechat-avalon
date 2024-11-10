01c94fd3512f10ce28151de6c9d33102a1b6444d
Feilong Song <songfl3@gmail.com>
2024-11-09 22:52:30 -0500

    This time should be able to talk

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -5,6 +5,7 @@ from wxcloudrun.dao import delete_counterbyid, query_counterbyid, insert_counter
 from wxcloudrun.model import Counters
 from wxcloudrun.response import make_succ_empty_response, make_succ_response, make_err_response
 
+from flask import Response
 import json
 
 
