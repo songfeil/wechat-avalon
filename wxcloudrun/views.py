29f23ad6c5b3934a4a26d35278a8b54ec5bd6df7
Feilong Song <songfl3@gmail.com>
2024-11-09 22:41:03 -0500

    Import json

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -5,6 +5,8 @@ from wxcloudrun.dao import delete_counterbyid, query_counterbyid, insert_counter
 from wxcloudrun.model import Counters
 from wxcloudrun.response import make_succ_empty_response, make_succ_response, make_err_response
 
+import json
+
 
 @app.route('/')
 def index():
